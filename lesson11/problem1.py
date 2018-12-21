6db93c9bbff0f2e8c60924f34893e5b3defd57e0
anayatzirojas <44092257+anayatzirojas@users.noreply.github.com>
2018-12-21 10:05:55 -0500

    first commit

---
+++ lesson11/problem1.py
@@ -0,0 +1,9 @@
+print('_'*65)
+print(" Iz iT bRiCk BoT?;")
+print()
+weather = input('Temperature in Fahrenheit:')
+if weather <= '32':
+	print('Itz bRiCk, wear a jacket dummy.')
+else:
+	print('YoU aII')
+print('-'*65)
